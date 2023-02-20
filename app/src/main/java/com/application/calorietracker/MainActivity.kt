32d0fffddae1478a29826ffbbb3aa2fbe1171dff
Sultan <sultan.ruet113054@gmail.com>
2023-02-20 17:45:55 +0600

    navigation util add done

--- app/src/main/java/com/application/calorietracker/MainActivity.kt
+++ app/src/main/java/com/application/calorietracker/MainActivity.kt
@@ -3,9 +3,12 @@ package com.application.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.application.calorietracker.ui.theme.CaloricTrackerTheme
+import com.application.core.navigation.Route
+import com.application.navigation.navigate
 import com.application.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -13,15 +16,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CaloricTrackerTheme {
-                WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.WELCOME
+                ) {
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE) {
+
+                    }
+                    composable(Route.GENDER) {
+
+                    }
+                    composable(Route.HEIGHT) {
+
+                    }
+                    composable(Route.WEIGHT) {
+
+                    }
+                    composable(Route.NUTRIENT_GOAL) {
+
+                    }
+                    composable(Route.ACTIVITY) {
+
+                    }
+                    composable(Route.GOAL) {
+
+                    }
+
+                    composable(Route.TRACKER_OVERVIEW) {
+
+                    }
+                    composable(Route.SEARCH) {
+
+                    }
+                }
             }
         }
     }
 }
 
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
--- onboarding/onboarding_presentation/src/main/java/com/application/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/application/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -8,15 +8,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.application.core.R
+import com.application.core.navigation.Route
+import com.application.core.util.UiEvent
 import com.application.core_ui.LocalSpacing
 import com.application.onboarding_presentation.components.ActionButton
 
-@Preview(showBackground = true)
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+) {
     val spacing = LocalSpacing.current
     Column(
         modifier = Modifier
@@ -26,14 +27,14 @@ fun WelcomeScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id =R.string.welcome_text),
+            text = stringResource(id = R.string.welcome_text),
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.h1
         )
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
-            text = stringResource(id =R.string.next),
-            onClick = { /*TODO*/ },
+            text = stringResource(id = R.string.next),
+            onClick = { onNavigate(UiEvent.Navigate(Route.AGE)) },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
