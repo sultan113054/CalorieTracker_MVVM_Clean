dd1fd231ef5e8403671dc9e60219e69b6fcec0ef
Sultan <sultan.ruet113054@gmail.com>
2023-02-21 17:02:49 +0600

    Tracker repository & Mappers add done

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -2,7 +2,7 @@ object ProjectConfig {
     const val appId = "com.android.calorytracker"
     const val compileSdk = 33
     const val jvmTarget = "1.8"
-    const val minSdk = 21
+    const val minSdk = 26
     const val targetSdk = 33
     const val versionCode = 1
     const val versionName = "1.0.1"
--- tracker/tracker_data/src/main/java/com/application/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/application/tracker_data/di/TrackerDataModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import com.application.tracker_data.local.TrackerDatabase
 import com.application.tracker_data.remote.OpenFoodApi
+import com.application.tracker_data.repository.TrackerRepositoryImpl
+import com.application.tracker_domain.repository.TrackerRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -50,5 +52,17 @@ object TrackerDataModule {
             TrackerDatabase::class.java,
             "tracker_db"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerRepository(
+        api: OpenFoodApi,
+        db: TrackerDatabase
+    ): TrackerRepository {
+        return TrackerRepositoryImpl(
+            dao = db.dao,
+            api = api
+        )
     }
 }
